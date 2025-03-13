11d53bec06481f1e232ae6e3483cb81d0f6b1572
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-03-13 17:58:18 +0530

    Update NewCollection.jsx

--- frontend/src/Components/NewCollection/NewCollection.jsx
+++ frontend/src/Components/NewCollection/NewCollection.jsx
@@ -8,7 +8,7 @@ const NewCollection = () => {
     const [new_collection,setNew_collection]=useState([]);
 
     useEffect(()=>{
-        fetch('http://localhost:4000/newcollection')
+        fetch('https://urbanmuse.onrender.com/newcollection')
         .then((response)=>response.json())
         .then((data)=>setNew_collection(data));
     },[])
