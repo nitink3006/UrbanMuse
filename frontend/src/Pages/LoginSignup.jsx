60d27adf4bf6b79ed64366aa8eb85663fd283bf1
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-03-13 18:09:01 +0530

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -20,7 +20,7 @@ const LoginSignup = () => {
     const login = async () =>{
         console.log("Login Function Executed",formData);
         let responseData;
-        await fetch ('http://localhost:4000/login',{
+        await fetch ('https://urbanmuse.onrender.com/login',{
             method:'POST',
             headers:{
                 Accept:'application/form-data',
@@ -43,7 +43,7 @@ const LoginSignup = () => {
     const signup = async () =>{
         console.log("SignUp Function Executed",formData);
         let responseData;
-        await fetch ('http://localhost:4000/signup',{
+        await fetch ('https://urbanmuse.onrender.com/signup',{
             method:'POST',
             headers:{
                 Accept:'application/form-data',
